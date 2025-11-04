20a654a568c196f3c6ca270092818d0dd6015ec8
nikolag1jic93430-lab <nikolag1jic93430@gmail.com>
2025-11-04 10:33:33 +0100

    Add files via upload

---
+++ pointers_arrays_strings/8-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int array[5] = {98, 402, -198, 298, -1024};
+
+	print_array(array, 5);
+
+	return (0);
+}
