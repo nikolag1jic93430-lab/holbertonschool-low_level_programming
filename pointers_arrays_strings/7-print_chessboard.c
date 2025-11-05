ce9028c1a6cec4b4d13a7071a3b4c72ec4619277
Nikola <vediumtv93@gmail.Com>
2025-11-05 13:50:08 +0000

    pmo

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints the chessboard
- * @a: array representing the board
+ * print_chessboard - affiche un échiquier de 8x8
+ * @a: tableau représentant l'échiquier
  *
- * Return: void
+ * Description: Cette fonction parcourt un tableau 8x8 de caractères
+ * et affiche chaque élément avec _putchar, ligne par ligne.
  */
 void print_chessboard(char (*a)[8])
 {
@@ -12,10 +13,10 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-		}
-		_putchar('\n');
+	for (j = 0; j < 8; j++)
+	{
+	_putchar(a[i][j]);
+	}
+	_putchar('\n');
 	}
 }
